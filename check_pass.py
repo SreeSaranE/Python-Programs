c1fd545882da30dce1ee2403f1dbec073ed724fb
SREESARAN E <145699613+SreeSaranE@users.noreply.github.com>
2025-05-15 12:08:29 +0530

    Update check_pass.py
    a small updates
    1. handles empty input
    2. displays the index of password if common password is used
    3. user input line is moved into main() function for more efficiency

--- check_pass.py
+++ check_pass.py
@@ -3,15 +3,16 @@ def check_pass(password: str):
         pass_list: list[str] = file.read().splitlines()
         #print(pass_list)
         if (password in pass_list):
-            print("Common pass")
+            print("Common pass"+':', pass_list.index(password))
         elif (password not in pass_list):
             print("Not a common Password")
         else:
             print("Enter a valid input")
-    
-user_pass: str = input("Enter your password: ")
 
 def main():
+    user_pass: str = input("Enter your password: ")
+    while user_pass == '':
+        user_pass: str = input("Enter your password: ")
     check_pass(user_pass)
 
 if __name__ == '__main__':
