3aaf77a7e5ffeb5cc43a90638b6573372fdcaeac
SREESARAN E <145699613+SreeSaranE@users.noreply.github.com>
2025-05-16 18:21:51 +0530

    Update check_pass.py
    made a little more advanced level password checker
    updates made:
    1. add a function check for character length
    2. add a function check for Special Character
    3. add a function check for Case sensitive

--- check_pass.py
+++ check_pass.py
@@ -1,3 +1,6 @@
+import string
+punch = [char for char in string.punctuation]
+
 def check_pass(password: str):
     with open('password.txt', 'r',encoding="utf-8-sig") as file:
         pass_list: list[str] = file.read().splitlines()
@@ -5,9 +8,27 @@ def check_pass(password: str):
         if (password in pass_list):
             print("Common pass"+':', pass_list.index(password))
         elif (password not in pass_list):
-            print("Not a common Password")
-        else:
-            print("Enter a valid input")
+            pass_len: int = len(password)
+            if (pass_len <= 8):
+                print(f"🟥The lenght of the password({password}) is too small!")
+            else:
+                print("🟩The pass has enough characters")
+            pass_split = [char for char in password]
+            special_check = 0
+            case_check = 0
+            for i in pass_split:
+                if i.isupper():
+                    case_check = 1
+                for j in punch:
+                    if i == j:
+                        print(f"🟩Your password({password}) contain special characters")
+                        special_check = 1
+            if (special_check != 1):
+                print(f"🟥Your password({password}) doesn't contain any special characters")
+            if (case_check != 1):
+                print(f"🟥Your password({password}) doesn't contain any Upper Case")
+            else:
+                print(f"🟩Your password({password})contain Upper Case")
 
 def main():
     user_pass: str = input("Enter your password: ")
