64a9a657b8b4c5be69835ddc39b7cb091ba4fef7
SREESARAN E <145699613+SreeSaranE@users.noreply.github.com>
2025-05-20 21:19:24 +0530

    Update crack_password.py
    updates:
    1. included time to check time taken to crack password
    2. removed unwanted lines of code
    3. optimized to complete faster

--- crack_password.py
+++ crack_password.py
@@ -1,10 +1,9 @@
+import time
 import string
-
-#alpha = string.ascii_letters+string.digits+string.punctuation
 
 def cap_check(user_password:str):
     for i in user_password:
-        if i.isupper:
+        if i.isupper():
             return True
         
 def punch_check(user_password:str):
@@ -17,39 +16,32 @@ def digits_check(user_password:str):
         if i in string.digits:
             return True
 
-'''
-alpha_all = string.ascii_letters+string.digits+string.punctuation
-alpha = [char for char in alpha_all]
-'''
-
 alpha = string.ascii_lowercase
 password: str = ''
 repeat: int = 0
 
-#user_password = "saran"
 user_password = input("Enter password: ")
+
+start_time = time.time()
+
 if cap_check(user_password):
     alpha = alpha + string.ascii_uppercase
 if punch_check(user_password):
     alpha = alpha + string.punctuation
 if digits_check(user_password):
     alpha = alpha + string.digits
-#alpha = alpha+digits_check(user_password)
+
 user_password_len = len(user_password)
-#user_password_text = [char for char in user_password]
 
 while password != user_password:
-    print("-----------------")
     for i in user_password:
         for j in alpha:
-            print(i,j, "not found")
             if j=='a':
                 repeat+=1
             if i==j:
                 password = password+j
-                print(i, "found")
-                print(password,'\n')
-                print(repeat)
+                print(password)
 
-print(password)
-print(repeat)
+end_time = time.time()
+total_time = "{:.3f}".format(end_time - start_time)
+print(f"Found your password({password}) in {repeat} iterations within {total_time}sec")
