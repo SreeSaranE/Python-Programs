0d2e0ade160368c26b71276caf79807c1f67a7ba
SREESARAN E <145699613+SreeSaranE@users.noreply.github.com>
2025-05-28 17:03:21 +0530

    Update Pomodoro.py
    Update made:
    1. able to see the running time on the compiler terminal.

--- starter_project/Pomodoro.py
+++ starter_project/Pomodoro.py
@@ -12,31 +12,30 @@ class Pomodoro:
         try:
             self.load_time = int(self.time_enter.get())
         except ValueError:
-            self.time_display.configure(text="Invalid Input")
+            self.time_display.configure(text ="Invalid Input!")
             return
         self.countdown()
-
+    
     def countdown(self):
         if self.load_time > 0:
-            mins, sec = divmod(self.load_time, 60)
-            formatted_time = f"{mins:02}:{sec:02}"
-            self.time_display.configure(text=f"Time: {formatted_time}")
+            mins, sec = divmod(self.load_time,60)
+            formet_time = f"{mins}:{sec}"
+            self.time_display.configure(text =f"Time: {formet_time}")
+            print(f"Time: {formet_time}")
             self.load_time -= 1
             self.window.after(1000, self.countdown)
         else:
-            self.time_display.configure(text="TimeOut")
-
-
+            self.time_display.configure(text =f"TimeOut")
+        #    self.window._set_appearance_mode("dark")
 
     def get_input(self):
         self.time_enter= ctk.CTkEntry(self.window,text_color='black', placeholder_text="Enter time in sec", placeholder_text_color='black')
         self.time_enter.pack(pady=20)
-        self.time_enter.insert(0,0)
 
         self.press_button = ctk.CTkButton(self.window, text="Start", corner_radius=50, command= self.timer)
         self.press_button.pack(pady=20)
 
-        self.time_display = ctk.CTkLabel(self.window, text="Timer: 0")
+        self.time_display = ctk.CTkLabel(self.window, text="Timer: 0", width=100, height= 30)
         self.time_display.pack(pady=20)
 
 
