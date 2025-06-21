d08adfe2f32ec0f72df4f58b6bf5380bf00bf859
SREESARAN E <145699613+SreeSaranE@users.noreply.github.com>
2025-06-21 21:36:10 +0530

    Create public_API.py
    1st step in creating public API using flask.
    created:
    1. index
    2. me
    need to improve:
    1. make it public
    2. add functionality

---
+++ intermediate_project/public_API.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return ("Hello World")
+
+@app.route('/me')
+def me():
+    user_name = request.args.get('name', type=str,default='Batman')
+
+    if isinstance(user_name, str):
+        return f"Hello {user_name}"
+    else:
+        return "Enter a valid input"
+
+if __name__ == '__main__':
+    app.run()
