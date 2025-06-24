c6f828521a47d6e4becd54d40b0d7c01cb737252
SREESARAN E <145699613+SreeSaranE@users.noreply.github.com>
2025-06-24 17:18:36 +0530

    Update Currency_Converter.py
    updates:
    1. automatically fetch data every day.
    2. integrate try exception to handle errors.
    3. handle invalid input.

--- intermediate_project/Currency_Converter.py
+++ intermediate_project/Currency_Converter.py
@@ -1,42 +1,63 @@
 import requests
 from typing import Final
 import json
+import datetime
 
-API_KEY: Final[str] = 'b3f448b3037e176b79d4efd637ec8781'
+API_KEY: Final[str] = 'bf8f82bf9880f304078eb9bc83de32fe'
 Base_URL: Final[str] = 'http://api.exchangeratesapi.io/v1/latest'
 
-def get_rates(mock: bool = False) -> dict:
-    if mock:
-        with open('rates.json', 'r') as file:
-            return json.load(file)
-    
-    payload: dict = {'access_key': API_KEY}
-    request = requests.get(url=Base_URL, params=payload)
-    data: dict = request.json()
-    with open('rates.json', 'w') as file:
-        json.dump(data, file)
-    return data
+def get_rates() -> dict:
+    with open('rates.json', 'r') as file:
+        data =  json.load(file)
+        old_date = data["date"]
+        today = str(datetime.date.today())
+        old_string = old_date.replace("-", "")
+        today_string = today.replace("-", "")
+        if old_string < today_string:
+            payload: dict = {'access_key': API_KEY}
+            request = requests.get(url=Base_URL, params=payload)
+            web_data: dict = request.json()
+            with open('rates.json', 'w') as file:
+                json.dump(web_data, file)
+            rate = data.get('rates')
+            return rate
+        else:
+            rate = data.get('rates')
+            return rate
 
 def get_currency(currency: str, rates: dict):
-    currency: str = currency.upper()
     if currency in rates.keys():
-        return (rates.get(currency))
+        return (rates[currency])
     else:
         raise ValueError(f"{currency} is not a valid Currency!")
     
-def convert_currecy(amount: float, base: str, vs: str, rates: dict):
+def convert_currency(amount: float, base: str, vs: str, rates: dict):
     base_rate: float = get_currency(base, rates)
     vs_rate: float = get_currency(vs, rates)
     conver: float = round((vs_rate/base_rate)*amount,2)
     print(f"{amount:,.2f} {base} = {conver:,.2f} {vs}")
 
 def main():
-    data = get_rates(mock=True)
-    rate = data.get('rates')
-    amount = int(input("Enter the amount: "))
-    base_curr = input("Enter base Currency: ")
-    vs_curr = input("Enter vs Currency: ")
-    convert_currecy(amount, base_curr, vs_curr, rate)
+    data = get_rates()
+    while True:
+        try:
+            amount = int(input("Enter the amount: "))
+        except ValueError:
+            print("Enter a valid Currency!")
+        else:
+            break
+
+    base_curr = (input("Enter base Currency: ")).upper()
+    vs_curr = (input("Enter vs Currency: ")).upper()
+    while True:
+        if base_curr not in data.keys():
+            base_curr = (input("Enter a valid base Currency: ")).upper()
+        elif vs_curr not in data.keys():
+            vs_curr = (input("Enter a valid vs Currency: ")).upper()
+        else:
+            break
+
+    convert_currency(amount, base_curr, vs_curr, data)
 
 if __name__ == '__main__':
     main()
